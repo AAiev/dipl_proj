daf1c925e3296e21b440ea1cfea2a52eabc8a039
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2024-05-17 16:00:37 +0300

    fix constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Файл с используемыми переменными
+
+# сайт для парсинга
+URL_PARSING = 'https://goldapple.ru/parfjumerija'
+URL_PARSING_START_PAGE = 'https://goldapple.ru/'
+
+# классы стилей объектов-товаров на страницах списков товаров, которые парсим
+HTML_CLASSES_ON_PRODUCT_LIST_PAGE = [
+    '_9Bnes wEteb h92Dk rMYg4',
+    'TjeQ6 _8l-0A Ks4bP VGaZJ',
+    '_2uZNh _0wwKr Ol19w RWNxw',
+]
+
+# Название файла .csv со списком ссылок на все товары
+FILE_CSV_WITH_LINKS_PRODUCTS = 'products_links.csv'
